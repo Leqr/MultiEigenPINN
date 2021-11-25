f947b8e98274e57c95a778f8868041d016eb37a3
Leonard Equer <lequer@eu-login-17.euler.ethz.ch>
2021-11-25 11:37:16 +0100

    hypersolve script for euler launch

---
+++ SamPINN/hypersolve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$1" = "single" ]; then
+    echo "Single core job"
+    bsub -oo "eigensolvesingle.out" -n 48 -W 02:00  -R "span[ptile=48]" -M 8G "python MultiSolve.py"
+fi
+
+if [ "$1" = "multi" ]; then
+    echo "Multi core job"
+    bsub -oo "eigensolvemulti.out" -n 48 -W 05:00 -R "span[ptile=6]" -M 8G "source ~/EigenPINN/vpinn/bin/activate && python MultiSolve.py"
+fi
+
+#-R "select[model==EPYC_7742]"
