50aefd406102b26a68744ba24b3b478c1858016c
Leonard Equer <lequer@eu-login-08.euler.ethz.ch>
2021-11-27 13:42:41 +0100

    mod sh

--- SamPINN/hypersolve.sh
+++ SamPINN/hypersolve.sh
@@ -6,7 +6,7 @@ fi
 
 if [ "$1" = "multi" ]; then
     echo "Multi core job"
-    bsub -oo "eigensolvemulti.out" -n 48 -W 05:00 -R "span[ptile=6]" -M 8G "source ~/EigenPINN/vpinn/bin/activate && python MultiSolve.py"
+    bsub -oo "eigensolvemulti.out" -n 48 -W 04:00 -R "span[ptile=3]" -M 8G "python MultiSolve.py"
 fi
 
 #-R "select[model==EPYC_7742]"
