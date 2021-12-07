68352ac18eb11ec0331fcc75cfad553d907fc5e5
Leonard Equer <lequer@eu-login-20.euler.ethz.ch>
2021-12-07 14:30:03 +0100

    mod

--- SamPINN/hypersolve.sh
+++ SamPINN/hypersolve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$1" = "single" ]; then
     echo "Single core job"
-    bsub -oo "eigensolvesingle.out" -n 48 -W 08:00  -R "span[ptile=48]" -M 8G "python MultiSolve.py"
+    bsub -oo "eigensolvesingle.out" -n 48 -W 06:00  -R "span[ptile=48]" -M 8G "python MultiSolve.py"
 fi
 
 if [ "$1" = "multi" ]; then
     echo "Multi core job"
-    bsub -oo "eigensolvemulti.out" -n 48 -W 04:00 -R "span[ptile=12]" -M 8G "python MultiSolve.py"
+    bsub -oo "eigensolvemulti.out" -n 48 -W 06:00 -R "span[ptile=12]" -M 8G "python MultiSolve.py"
 fi
 
 #-R "select[model==EPYC_7742]"
